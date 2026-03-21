97bfa3b0cc11a9f300fd94c30cce3b3cad486407
Sohan Poojari <sohan7530@gmail.com>
2026-03-21 23:37:40 +0530

    Phase 2 Complete: All services containerized and orchestrated with Docker Compose

--- services/order-service/app/main.py
+++ services/order-service/app/main.py
@@ -1,11 +1,12 @@
 import requests
 from fastapi import FastAPI, HTTPException
+import os
 
 app = FastAPI(title="Order Service API", version="1.0.0")
 
-# These are the local addresses of our other two services
-USER_SERVICE_URL = "http://localhost:8001"
-PRODUCT_SERVICE_URL = "http://localhost:8002"
+# Fetching the Docker Network URLs passed by Docker Compose!
+USER_SERVICE_URL = os.getenv("USER_SERVICE_URL", "http://user-service:8001")
+PRODUCT_SERVICE_URL = os.getenv("PRODUCT_SERVICE_URL", "http://product-service:8002")
 
 @app.get("/health")
 def health_check():
@@ -13,24 +14,18 @@ def health_check():
 
 @app.post("/create-order/{user_id}/{product_id}")
 def create_order(user_id: int, product_id: int):
-    # 1. Ask User Service: Does this user exist?
     user_response = requests.get(f"{USER_SERVICE_URL}/users/{user_id}")
     if user_response.status_code != 200:
         raise HTTPException(status_code=404, detail="User not found")
     
-    # 2. Ask Product Service: Does this product exist?
     prod_response = requests.get(f"{PRODUCT_SERVICE_URL}/products/{product_id}")
     if prod_response.status_code != 200:
         raise HTTPException(status_code=404, detail="Product not found")
 
-    user_data = user_response.json()
-    product_data = prod_response.json()
-
-    # 3. If both exist, create a combined response (The "Order")
     return {
         "order_id": 999,
-        "customer": user_data["username"],
-        "item": product_data["name"],
-        "price_paid": product_data["price"],
-        "status": "Order Placed Successfully"
+        "customer": user_response.json()["username"], 
+        "item": prod_response.json()["name"],         
+        "price_paid": prod_response.json()["price"],
+        "status": "Order Placed Successfully via Docker Network!"
     }
